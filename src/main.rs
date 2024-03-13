daf6541517136ddfe3cce343b1b73ad55d4f61ff
Lemon <bxb100@outlook.com>
2024-03-13 21:56:56 +0800

    feat: support direct generate commit msg by pipe diff

--- src/main.rs
+++ src/main.rs
@@ -13,11 +13,13 @@ mod summarize;
 mod toml;
 mod util;
 
+use crate::actions::prepare_commit_msg::generate_commit_message;
 use anyhow::Result;
 use clap::Parser;
 use log::LevelFilter;
 use settings::Settings;
 use simple_logger::SimpleLogger;
+use std::io::Read;
 
 use crate::cli::Action;
 
@@ -39,12 +41,23 @@ async fn main() -> Result<()> {
     let settings = Settings::new()?;
     debug!("Settings: {:?}", settings);
 
-    match cli_args.action {
-        Action::Config(cli_args) => actions::config::main(settings, cli_args).await,
-        Action::Install => actions::install::main(settings).await,
-        Action::Uninstall => actions::uninstall::main(settings).await,
-        Action::PrepareCommitMsg(cli_args) => {
-            actions::prepare_commit_msg::main(settings, cli_args).await
+    if let Some(subcommand) = cli_args.action {
+        match subcommand {
+            Action::Config(cli_args) => actions::config::main(settings, cli_args).await,
+            Action::Install => actions::install::main(settings).await,
+            Action::Uninstall => actions::uninstall::main(settings).await,
+            Action::PrepareCommitMsg(cli_args) => {
+                actions::prepare_commit_msg::main(settings, cli_args).await
+            }
         }
+    } else {
+        let stdin = std::io::stdin();
+        let mut buffer = String::new();
+        let mut handle = stdin.lock();
+
+        handle.read_to_string(&mut buffer)?;
+        let commit_msg = generate_commit_message(&settings, &buffer).await?;
+        print!("{commit_msg}");
+        Ok(())
     }
 }
